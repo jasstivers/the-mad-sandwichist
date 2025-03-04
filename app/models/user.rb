864b7890e4b0dc09583cea37f43f6599848ab8ed
Jasmine Stivers <jasstivers@gmail.com>
2025-03-04 22:03:01 +0900

    sandwich making badges work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_merit
+  has_many :sandwiches
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   acts_as_favoritor
---
+++ config/initializers/merit.rb
@@ -0,0 +1,64 @@
+# Use this hook to configure merit parameters
+Merit.setup do |config|
+  # Check rules on each request or in background
+  # config.checks_on_each_request = true
+
+  # Add application observers to get notifications when reputation changes.
+  # config.add_observer 'MyObserverClassName'
+
+  # Define :user_model_name. This model will be used to grant badge if no
+  # `:to` option is given. Default is 'User'.
+  # config.user_model_name = 'User'
+
+  # Define :current_user_method. Similar to previous option. It will be used
+  # to retrieve :user_model_name object if no `:to` option is given. Default
+  # is "current_#{user_model_name.downcase}".
+  # config.current_user_method = 'current_user'
+end
+
+# Create application badges (uses https://github.com/norman/ambry)
+# Rails.application.reloader.to_prepare do
+#   badge_id = 0
+#   [{
+#     id: (badge_id = badge_id+1),
+#     name: 'just-registered'
+#   }, {
+#     id: (badge_id = badge_id+1),
+#     name: 'best-unicorn',
+#     custom_fields: { category: 'fantasy' }
+#   }].each do |attrs|
+#     Merit::Badge.create! attrs
+#   end
+# end
+
+Rails.application.reloader.to_prepare do
+  Merit::Badge.create!(
+    id: 1,
+    name: "The Journey Begins",
+    description: "Created your first sandwich!",
+  )
+
+  Merit::Badge.create!(
+    id: 2,
+    name: "Earl of Sandwich",
+    description: "Created 10 sandwiches!",
+  )
+
+  Merit::Badge.create!(
+    id: 3,
+    name: "Tastemaker",
+    description: "Have a sandwich reach 4 stars with 50 reviews!",
+  )
+
+  Merit::Badge.create!(
+    id: 4,
+    name: "B.L.Team!",
+    description: "Created a sandwich with bacon, lettuce, and tomato!",
+  )
+
+  Merit::Badge.create!(
+    id: 5,
+    name: "Bun to be Wild",
+    description: "Created a sandwich with a only bread!",
+  )
+end
