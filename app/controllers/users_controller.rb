43d2d05d2c8bcfa580d880561d6dffa80389a903
Jasmine Stivers <jasstivers@gmail.com>
2025-03-06 00:41:02 +0900

    all trophies displayed and unearned are faded and lose a check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,10 @@ class UsersController < ApplicationController
 
   def show
       @user = User.find(params[:id])
+      # @unearned_badges = @user.badges.map { |badge|
+      #   badge.where(earned: false)
+      # }
+      @allbadges = Merit::Badge.all
+      @userbadges = @user.badges
   end
 end
